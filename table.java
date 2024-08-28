131c8ef9d1e1ff803d376bee7133b3a56aa1f99a
vaishn-stack <vaishnavimorankar296@gmail.com>
2024-08-28 13:48:51 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,20 @@
+package vaishu;
+import java.util.Scanner;
+
+public class table {
+
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number:");
+		int num=sc.nextInt();
+		{
+			for(int i=1;i<=10;i++)
+			{
+				System.out.println(num+"*"+i+"="+num*i);
+			}
+		}
+		
+	}
+
+}
